9fcccffe3eb4d6d0da4697c8aebf0f533db27c40
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-04-22 11:13:51 -0700

    update

---
+++ src/components/StraightLinesBox/StraightLinesBox.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import './StraightLinesBox.css'
+
+const StraightLinesBox = ({ classForAnimation }) => {
+
+
+    return (
+        <React.Fragment>
+            <div className={`landingPage_lineBox ${classForAnimation}`}>
+
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default StraightLinesBox;
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,64 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import './LandingPage.css'
 import NavBar from '../../components/NavBar/NavBar'
-import ImageWrapper from '../../components/ImageWrapper/ImageWrapper'
-import img1 from '../../assets/images/4e76867b-7651-4778-917b-38d3e4b17f78_Render_01+3.png'
-import img2 from '../../assets/images/f8ac95a6-d5a5-489a-97da-564bf60ba010_image+1.png'
+import StraightLinesBox from '../../components/StraightLinesBox/StraightLinesBox'
 const LandingPage = () => {
     const [showLabelBox, setShowLabelBox] = useState(false);
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowLabelBox(true)
-        }, 2000)
+        }, 4000)
         return () => clearTimeout(timer)
     }, [])
     return (
         <React.Fragment>
             <div className="landingPage_box">
                 <div className="landing_section">
-                    <NavBar />
-                    <h1 className='landingPage_mainHeading'>Garoa Skincare</h1>
-                    <ImageWrapper
-                        imageSrc={img1}
-                        height="33rem"
-                        width="100%"
-                        classForAnimation="main_image"
-                    />
-                    <div className={`landingPage_labelBox ${showLabelBox ? 'show' : ''}`}>
-                        <div>
-                            <p>
-                                Client : Garoa Skincare<br />
-                                Project : Branding & Website<br />
-                                Year : 2021<br />
-                            </p>
+                    {showLabelBox && <NavBar />}
+                    <StraightLinesBox />
+                    <div className="landingPage_paraBox">
+                        <div className='small_para'>
+                            {showLabelBox && <p>
+                                N 40°14`2.16"<br />
+                                W 26°12`13.4"
+                            </p>}
                         </div>
-                        <div>
-                            <p>
-                                Disciplines
-                            </p>
-                            <p className='disciplines'>
-                                Branding<br />
-                                Web Design<br />
-                                Development<br />
-                                Photography<br />
-                            </p>
+                        <div className='big_para'>
+                            {showLabelBox && <p>
+                                Studio-BA is<br />
+                                a multidisciplinary design practice.
+                            </p>}
                         </div>
                     </div>
                 </div>
-                <div className="landingPage_linkContainer">
-                    <div ></div>
-                    <div>
-                        <a href="" className='landingPage_link'>Visit live site↗</a>
-                    </div>
-                </div>
-                <ImageWrapper
-                    imageSrc={img2}
-                    height="33.8rem"
-                    width="100%"
-                    classForAnimation="second_image"
-                />
+
             </div>
-        </React.Fragment>
+        </React.Fragment >
     )
 }
 
