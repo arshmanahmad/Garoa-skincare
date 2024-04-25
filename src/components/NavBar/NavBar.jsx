1cf9729f60c24fd062ee4283b57244dbbd737025
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-04-25 09:41:30 -0700

    update

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './NavBar.css'
 
 const NavBar = ({ classForChange }) => {
+    const [changeText, setChangeText] = useState(false);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setChangeText(true)
+        }, 4000)
+        return () => clearTimeout(timer)
+    }, [])
     return (
         <React.Fragment>
             <div className={`navBar_box ${classForChange}`}>
-                <p className='first_link'>STUDIO-BA®</p>
+                <p className='first_link'>STUDIO-<span className='changeText'>{changeText ? "BA®" : "BRUNZOARIZIO®"}</span></p>
                 <p>Sao Paulo 11:56 AM</p>
                 <p>London 02:57 PM</p>
                 <p className='last_link'>Summary</p>
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './LandingPage.css';
 import NavBar from '../../components/NavBar/NavBar'
 import StraightLinesBox from '../../components/StraightLinesBox/StraightLinesBox'
@@ -7,88 +7,108 @@ import CaptionBar from '../../components/CaptionBar/CaptionBar'
 import VerticalBar from '../../components/VerticalBar/VerticalBar';
 const LandingPage = () => {
     const [showLabelBox, setShowLabelBox] = useState(false);
-    const [scrollPosition, setScrollPosition] = useState(0);
+    const [scrollPosition, setScrollPosition] = useState(1);
+
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowLabelBox(true)
         }, 4000)
         return () => clearTimeout(timer)
     }, [])
-    const handleWheelScroll = (evt) => {
-        evt.preventDefault();
-        setScrollPosition(scrollPosition + evt.deltaY);
-    };
+
+    useEffect(() => {
+        const handleWheelScroll = (evt) => {
+            evt.preventDefault();
+            setScrollPosition(prevScrollPosition => prevScrollPosition + evt.deltaY);
+        };
+        const handleKeyDown = (evt) => {
+            const scrollStep = 150;
+            if (evt.key === 'ArrowUp' || evt.key === 'ArrowRight') {
+
+                setScrollPosition(prevScrollPosition => prevScrollPosition + scrollStep);
+            } else if (evt.key === 'ArrowDown' || evt.key === 'ArrowLeft') {
+                setScrollPosition(prevScrollPosition => prevScrollPosition - scrollStep);
+            }
+        };
+        window.addEventListener("wheel", handleWheelScroll, { passive: false });
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {
+            window.removeEventListener("wheel", handleWheelScroll);
+            window.removeEventListener("keydown", handleKeyDown);
+
+        };
+    }, [scrollPosition, []]);
+
     return (
         <React.Fragment>
-            <div
-                id='container'
-                className="landingPage_box"
-                onWheel={handleWheelScroll}
-                style={{ transform: `translateX(-${scrollPosition}px)` }}
-            >
-                <div className="landingPage_section1">
-                    {showLabelBox && <NavBar />}
-                    {showLabelBox && <AnimatedCircles />}
-                    <StraightLinesBox />
-                    <div className="landingPage_paraBox">
-                        <div className='small_para'>
-                            {showLabelBox && <p>
-                                N 40°14`2.16"<br />
-                                W 26°12`13.4"
-                            </p>}
-                        </div>
-                        <div className='big_para'>
-                            {showLabelBox && <p>
-                                <span className='nanoSized_para'>(Working Worldwide)</span>Studio-BA is<br />
-                                a multidisciplinary design practice.
-                            </p>}
-                        </div>
-                    </div>
-                </div>
-                <div className="landingPage_section2">
-                    {showLabelBox && <NavBar />}
-                    <div className="caption_container">
-                        <CaptionBar text="FOREWORD" number="3" />
-                        <CaptionBar text="APPROACH" number="5" />
-                        <CaptionBar text="PROCESS" number="6" />
-                        <CaptionBar text="SELECTED PROJECTS" number="12" />
-                        <CaptionBar text="AWARDS & RECOGNITIONS" number="22" />
-                        <CaptionBar text="CONTACT US" number="24" />
-                        <CaptionBar text="COLOPHON" number="25" />
-                    </div>
-                    <p className='bigHeading'>SUMMARY</p>
-                </div>
-                <div className="landingPage_section3">
-                    {showLabelBox && <NavBar />}
-                    <div className="vertical_barContainer">
-                        <VerticalBar text="FOREWORD" number="3" />
-                    </div>
-                    <div className="section3_parasContainer">
-                        <div className="section3_para1">
-                            <p>
-                                Bruno is currently part of the Jury board for two of the most renowned awards in the industry Awwwards and Webby Awards.
-                            </p>
-                        </div>
-                        <div className="section3_para2">
-                            <p>
-                                Studio—BA® is an award-winning design and art direction practice working with a broad range of clients across a mix of disciplines that includes web design, creative direction, graphic design, and photography together with an extensive community of collaborators: photographers, 3d artists, and creative developers to deliver bespoke design projects. With an international client list,  the practice can work alone or in collaboration with design agencies,   and bureaus, past experiences include partnerships with Jam3, Sapient Razorfish and Publicis Groupé.
-                            </p>
-                        </div>
-                        <div className="section3_para3">
-                            <p>
-                                Since the start of the practice in 2010, Bruno has received awards and nominations from The FWA, Awwwards, Webby Awards, Visuelle, CSS Design Awards, AIGA, Typewolf, Behance, Muzli, and others.   Bruno is frequently called to be a part of jury boards of world renowned creative competitions. Currently a proud juror of the Webby Awards (The International Academy of Digital Arts and Sciences®) and Awwwards.
-                            </p>
+            {showLabelBox && <NavBar />}
+            <div style={{ overflowX: 'auto', overflowX: "hidden", whiteSpace: 'nowrap', width: '100%', height: '100vh' }}>
+                <div
+                    id='container'
+                    className="landingPage_box"
+                    style={{ width: '500vw', height: '100%', backgroundColor: '#f1efed', transform: `translateX(-${scrollPosition}px)` }}
+                >
+                    <div className="landingPage_section1">
+                        {showLabelBox && <AnimatedCircles />}
+                        <StraightLinesBox />
+                        <div className="landingPage_paraBox">
+                            <div className='small_para'>
+                                {showLabelBox && <p>
+                                    N 40°14`2.16"<br />
+                                    W 26°12`13.4"
+                                </p>}
+                            </div>
+                            <div className='big_para'>
+                                {showLabelBox && <p>
+                                    <span className='nanoSized_para'>(Working Worldwide)</span>Studio-BA is<br />
+                                    a multidisciplinary design practice.
+                                </p>}
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="landingPage_section4">
-                    {showLabelBox && <NavBar />}
-                    <div className="img_container">
+                    <div className="landingPage_section2">
+                        <div className="caption_container">
+                            <CaptionBar text="FOREWORD" number="3" />
+                            <CaptionBar text="APPROACH" number="5" />
+                            <CaptionBar text="PROCESS" number="6" />
+                            <CaptionBar text="SELECTED PROJECTS" number="12" />
+                            <CaptionBar text="AWARDS & RECOGNITIONS" number="22" />
+                            <CaptionBar text="CONTACT US" number="24" />
+                            <CaptionBar text="COLOPHON" number="25" />
+                        </div>
+                        <p className='bigHeading'>SUMMARY</p>
                     </div>
-                    <VerticalBar className="second_verticalBar" text="APPROACH" number="5" />
-                </div>
+                    <div
+                        className="landingPage_section3"
 
-
+                    >
+                        <div className="vertical_barContainer" >
+                            <VerticalBar text="FOREWORD" number="3" />
+                        </div>
+                        <div className="section3_parasContainer">
+                            <div className="section3_para1">
+                                <p>
+                                    Bruno is currently part<br /> of the Jury board for two<br /> of the most renowned<br /> awards in the <br />industry Awwwards<br /> and Webby Awards.
+                                </p>
+                            </div>
+                            <div className="section3_para2">
+                                <p>
+                                    Studio—BA® is an award-winning <br />design and art direction practice<br /> working with a broad range of<br /> clients across a mix of disciplines <br />that includes web design, creative<br /> direction, graphic design, and<br /> photography together with an <br />extensive community of collaborators<br />: photographers, 3d artists, and creative<br /> developers to deliver bespoke<br /> design projects. With an international<br /> client list,  the practice can<br /> work alone or in collaboration with design <br />agencies,   and bureaus, past experiences include<br /> partnerships with Jam3, Sapient<br /> Razorfish and Publicis Groupé.
+                                </p>
+                            </div>
+                            <div className="section3_para3">
+                                <p>
+                                    Since the start of the practice in 2010,<br /> Bruno has received awards and nominations<br /> from The FWA, Awwwards, Webby Awards,<br /> Visuelle, CSS Design Awards, AIGA, <br />Typewolf, Behance, Muzli, and others. <br />  Bruno is frequently called to be<br /> a part of jury boards of world<br /> renowned creative competitions. <br />Currently a proud juror of the <br />Webby Awards (The International Academy <br />of Digital Arts and Sciences®)<br /> and Awwwards.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="img_container">
+                        </div>
+                    </div>
+                    <div className="landingPage_section5">
+                        <VerticalBar className="second_verticalBar" text="APPROACH" number="5" />
+                    </div>
+                </div>
             </div>
         </React.Fragment >
     )
